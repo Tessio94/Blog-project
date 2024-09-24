681482c4a0477aa9cea032db82e0bfd064d29eb0
Tessio94 <simeklapan@gmail.com>
2024-09-24 18:43:51 +0200

    seconds commit

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Article = require("./../models/article");
 const router = express.Router();
 
 router.get("/new", (req, res) => {
@@ -6,7 +7,11 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  res.render("articles/new");
+  const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const articleRouter = require("./routes/articles");
+
+mongoose.connect("mongodb://localhost/blog");
 
 app.set("view engine", "ejs");
 
 app.use("/articles", articleRouter);
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   const articles = [
